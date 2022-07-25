664321c4a4ccc85c6ec4b6f37b5dc4d956c1ee45
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-07-25 09:33:07 +0500

    3. Int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager stats = new SalesManager(new int[]{5, -10, 15, -2, 3});
+        SalesManager stats = new SalesManager(new long[]{5, -10, 15, -2, 3});
         System.out.println("Максимальная продажа: " + stats.max());
     }
 }
