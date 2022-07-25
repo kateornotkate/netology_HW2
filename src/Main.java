788cd71cff0354b8693d82a3a202e9546cdc4132
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-07-25 09:38:03 +0500

    4. Average calculation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager stats = new SalesManager(new int[]{5, -10, 15, -2, 3});
+        SalesManager stats = new SalesManager(new int[]{10, 20, 15, -10, 5});
         System.out.println("Максимальная продажа: " + stats.max());
+        System.out.println("Минимальная продажа: " + stats.min());
+        System.out.println("Среднее: " + stats.average());
     }
 }
